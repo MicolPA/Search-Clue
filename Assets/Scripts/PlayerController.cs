5fbab87aa425361b69af040f912339c56620043c
Micol Peralta <micolpa08@gmail.com>
2023-01-05 14:42:51 -0400

    add: terrain and campfire zone

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 5.0f;
     public float turnSpeed = 10.0f;
-    private Rigidbody playerRb;
-    private Animator playerAnim;
-
-    public GameObject centerOfMass;
     public bool isOnGround = true;
     public float jumpForce;
+
+    public GameObject centerOfMass;
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public AudioClip footstepAudio;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +23,7 @@ public class PlayerController : MonoBehaviour
         playerRb.centerOfMass = centerOfMass.transform.position;
 
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
 
     }
 
@@ -28,17 +31,32 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // playerRb.
+
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+            speed *= 2;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift)){
+            speed /= 2;
+        }
+
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
         // transform.Translate(Vector3.up * speed * Time.deltaTime * horizontalInput);
+        
 
 
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up,  Time.deltaTime * turnSpeed * horizontalInput);
+            Debug.Log(verticalInput);
 
         if(verticalInput > 0){
+            Debug.Log("RUNNING");
+            // playerAudio.Stop();
             playerAnim.SetBool("Running_b", true);
         }else{
+            Debug.Log("STOP");
+            // playerAudio.Play();
             playerAnim.SetBool("Running_b", false);
         }
         // transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
